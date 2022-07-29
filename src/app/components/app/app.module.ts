760502dd49f5ca22ba12567f574ae41923ccc14d
Ziggleton <ziggleton@icloud.com>
2022-07-29 11:41:06 +0200

    Flesh out app component

--- src/app/components/app/app.module.ts
+++ src/app/components/app/app.module.ts
@@ -1,18 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { allIcons, HeroIconModule } from 'ng-heroicon';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { UsersModule } from '../pages/users/users.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { SideNavComponent } from '../side-nav/side-nav.component';
+import { UsersComponent } from '../pages/users/users.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'users',
+    component: UsersComponent,
+  },
+  { path: '**', component: UsersComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideNavComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    UsersModule,
+    HeroIconModule.withIcons(
+      {
+        ...allIcons,
+      },
+      {
+        defaultHostDisplay: 'block',
+        attachDefaultDimensionsIfNoneFound: true,
+      }
+    ),
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
