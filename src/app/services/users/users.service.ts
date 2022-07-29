97823cd1a7cb48b202f5442736f56e2bd1f7b49e
Ziggleton <ziggleton@icloud.com>
2022-07-29 11:44:54 +0200

    [WIP] Add user service

--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+export interface User {
+  userName: string;
+  userType: string;
+  profileImage: string;
+  fullName?: string;
+  bio?: string;
+  followers?: number;
+  following?: number;
+  repos?: number;
+  gists?: number;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
-
-  constructor() { }
+  // userList: of(User) = [];
+  // repoList: of(Repo) = [];
+  // constructor() {}
+  // getUsers: Observable<User[]> = () => {
+  //   return <User[]>[];
+  // };
+  // searchUsers: Observable<User[]> = (username: string) => {
+  //   console.log(username);
+  //   return <User[]>[];
+  // };
+  // getRepos: Observable<User[]> = (username: string) => {
+  //   return <User[]>[];
+  // };
+  // getGists: Observable<User[]> = (username: string) => {
+  //   return <User[]>[];
+  // };
+  // getFollowers: Observable<User[]> = (username: string) => {
+  //   return <User[]>[];
+  // };
+  // getFollowing: Observable<User[]> = (username: string) => {
+  //   return <User[]>[];
+  // };
 }
