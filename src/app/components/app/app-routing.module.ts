1b1b3d422a6a5b32f0f4d66aaefadcd1899563c2
Ziggleton <ziggleton@icloud.com>
2022-07-29 18:25:32 +0200

    Add routes

--- src/app/components/app/app-routing.module.ts
+++ src/app/components/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { UserDetailsComponent } from '../pages/user-details/user-details.component';
+import { UserNotFoundComponent } from '../pages/user-not-found/user-not-found.component';
+import { UsersComponent } from '../pages/users/users.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'users',
+    children: [
+      { path: '', component: UsersComponent },
+      { path: 'details/:username', component: UserDetailsComponent },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: '/users',
+    pathMatch: 'full',
+  },
+  { path: '**', component: UserNotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
