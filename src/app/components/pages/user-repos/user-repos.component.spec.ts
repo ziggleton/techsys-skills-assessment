31d17a02b2ff0f2f2e930327938b63c08ffe15d4
Ziggleton <ziggleton@icloud.com>
2022-07-29 11:43:32 +0200

    add user repo page

---
+++ src/app/components/pages/user-repos/user-repos.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserReposComponent } from './user-repos.component';
+
+describe('UserReposComponent', () => {
+  let component: UserReposComponent;
+  let fixture: ComponentFixture<UserReposComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UserReposComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UserReposComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/pages/user-repos/user-repos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-repos',
+  templateUrl: './user-repos.component.html',
+  styleUrls: ['./user-repos.component.scss']
+})
+export class UserReposComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
