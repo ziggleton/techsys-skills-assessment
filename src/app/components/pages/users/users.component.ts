2d5fb22f699c6c01bb1bcf51a23d65ad5a808918
Ziggleton <ziggleton@icloud.com>
2022-07-30 13:59:43 +0200

    Extend user component functionality

--- src/app/components/pages/users/users.component.ts
+++ src/app/components/pages/users/users.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ToggleService, UsersService } from 'src/app/services';
+import { User } from 'src/app/services/users/users.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  sub: Subscription;
+  userSub: Subscription;
+  canShowPreview: boolean = false;
+  selectedItem: User | null = null;
+  users: User[] = [];
+  headers: Array<string> = [
+    'profile image',
+    'username',
+    'user type',
+    'actions',
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private userService: UsersService,
+    private uiService: ToggleService
+  ) {
+    this.sub = this.uiService
+      .onShowPreview()
+      .subscribe((val: boolean) => (this.canShowPreview = val));
+    this.userSub = this.uiService
+      .onSelectItem()
+      .subscribe((val: User) => (this.selectedItem = val));
   }
 
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((users: any) => {
+      users.map((user: any) => {
+        this.users.push({
+          id: user.id,
+          userName: user.login,
+          userType: user.type,
+          profileImage: user.avatar_url,
+        });
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.users = [];
+    this.headers = [];
+    this.sub.unsubscribe();
+  }
 }
