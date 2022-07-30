a77c80afad2bbe989a836ce75efc6a9a5142cd67
Ziggleton <ziggleton@icloud.com>
2022-07-30 13:59:14 +0200

    Refactor user module

--- src/app/components/pages/users/users.module.ts
+++ src/app/components/pages/users/users.module.ts
@@ -1,13 +1,22 @@
-import { UserListComponent } from './../../user-list/user-list.component';
+import { UserListComponent } from '../../shared/user-list/user-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { allIcons, HeroIconModule } from 'ng-heroicon';
 import { FormsModule } from '@angular/forms';
 
 import { UsersComponent } from './users.component';
+import { AvatarComponent } from '../../shared/avatar/avatar.component';
+import { GridComponent } from '../../shared/grid/grid.component';
+import { PreviewComponent } from '../../shared/preview/preview.component';
 
 @NgModule({
-  declarations: [UsersComponent, UserListComponent],
+  declarations: [
+    UsersComponent,
+    UserListComponent,
+    AvatarComponent,
+    GridComponent,
+    PreviewComponent
+  ],
   imports: [
     FormsModule,
     BrowserModule,
