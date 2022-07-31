df587790eb67a28dfc2fc9cd915371720d929eec
Ziggleton <ziggleton@icloud.com>
2022-07-31 20:31:08 +0200

    add routes

--- src/app/components/app/app-routing.module.ts
+++ src/app/components/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { UserDetailsComponent } from '../pages/user-details/user-details.component';
+import { UserFollowersComponent } from '../pages/user-followers/user-followers.component';
+import { UserFollowingComponent } from '../pages/user-following/user-following.component';
+import { UserGistsComponent } from '../pages/user-gists/user-gists.component';
 import { UserNotFoundComponent } from '../pages/user-not-found/user-not-found.component';
+import { UserReposComponent } from '../pages/user-repos/user-repos.component';
 import { UsersComponent } from '../pages/users/users.component';
 
 const appRoutes: Routes = [
@@ -11,6 +15,10 @@ const appRoutes: Routes = [
     children: [
       { path: '', component: UsersComponent },
       { path: 'details/:username', component: UserDetailsComponent },
+      { path: 'repos/:username', component: UserReposComponent },
+      { path: 'gists/:username', component: UserGistsComponent },
+      { path: 'follower/:username', component: UserFollowersComponent },
+      { path: 'following/:username', component: UserFollowingComponent },
     ],
   },
   {
